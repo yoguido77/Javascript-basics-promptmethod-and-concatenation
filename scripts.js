72cfd496bdb3e6a708b34c3623e6fae7d6ba1d0e
yoguido77 <yoguido77@gmail.com>
2016-08-25 15:49:08 -0500

    working with prompt() method and concatenation

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var visitor = prompt('What is your name?');
+// use + sign to concatenate
+var message = 'Hello ' + visitor + ". Welcome to Treehouse.";
+message += "We are so glad that you came by to visit, ";
+message += visitor;
+message += ". Please come again, when you want to learn some more.";
+document.write(message);
+
